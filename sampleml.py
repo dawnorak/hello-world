a62cee303aa596f525deefcffa88480d580640a0
Sathi Vaigarai <94104558+dawnorak@users.noreply.github.com>
2023-08-29 12:07:14 +0530

    Create sampleml.py

---
+++ sampleml.py
@@ -0,0 +1,29 @@
+import tensorflow as tf
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Flatten, Dense
+from tensorflow.keras.optimizers import SGD
+
+# Load and preprocess the MNIST dataset
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+# Build a simple neural network model
+model = Sequential([
+    Flatten(input_shape=(28, 28)),
+    Dense(128, activation='relu'),
+    Dense(10, activation='softmax')
+])
+
+# Compile the model
+model.compile(optimizer=SGD(learning_rate=0.01),
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Train the model for a few epochs
+num_epochs = 5
+model.fit(x_train, y_train, epochs=num_epochs, validation_data=(x_test, y_test))
+
+# Evaluate the model on the test data
+test_loss, test_acc = model.evaluate(x_test, y_test)
+print(f"Test accuracy: {test_acc}")
